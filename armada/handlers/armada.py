0ce1f263defbcadb30edecb8194e3ae6ff99adb9
Erik N <neeleyea@gmail.com>
2017-10-04 20:34:59 +0000

    [WIP] fix(tiller): Adding option to view pod output log - Updated:         - handler/k8s         - handler/tiller         - cli/test         - armada manifest
    Closes #162
    
    Change-Id: I1e602f8aded4f78745e2577741652f17bf6f05ef

--- armada/handlers/armada.py
+++ armada/handlers/armada.py
@@ -53,6 +53,7 @@ class Armada(object):
                  timeout=DEFAULT_TIMEOUT,
                  tiller_host=None,
                  tiller_port=44134,
+                 output=False,
                  values=None):
         '''
         Initialize the Armada Engine and establish
@@ -210,7 +211,10 @@ class Armada(object):
 
             desc = entry.get('description', 'A Chart Group')
             chart_group = entry.get(const.KEYWORD_CHARTS, [])
-            test_charts = entry.get('test_charts', False)
+
+            test_charts = entry.get('test_charts', {}).get('enabled', False)
+            test_charts_output = entry.get('test_charts', {}).get(
+                'output', False)
 
             if entry.get('sequenced', False) or test_charts:
                 chart_wait = True
@@ -220,7 +224,10 @@ class Armada(object):
             for gchart in chart_group:
                 chart = dotify(gchart['chart'])
                 values = gchart.get('chart').get('values', {})
-                test_chart = gchart.get('chart').get('test', False)
+                test_chart = gchart.get('chart').get('test', {}).get(
+                    'enabled', False)
+                test_chart_output = gchart.get('chart').get('test', {}).get(
+                    'output', False)
                 pre_actions = {}
                 post_actions = {}
 
@@ -330,11 +337,12 @@ class Armada(object):
 
                 if test_charts or test_chart:
                     LOG.info('Testing: %s', prefix_chart)
-                    resp = self.tiller.testing_release(prefix_chart)
-                    test_status = getattr(resp.info.status,
-                                          'last_test_suite_run', 'FAILED')
+                    output = test_charts_output or test_chart_output
+                    resp = self.tiller.testing_release(
+                        prefix_chart, output=output)
+                    test_status = resp['Status']
                     LOG.info("Test INFO: %s", test_status)
-                    if resp:
+                    if test_status[0] == 'Test Passed':
                         LOG.info("PASSED: %s", prefix_chart)
                     else:
                         LOG.info("FAILED: %s", prefix_chart)
